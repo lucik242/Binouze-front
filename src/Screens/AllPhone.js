1c7c674ded99589a3d959ae857db2802b69b4632
KK <kula.jega@gmail.com>
2019-11-15 21:06:35 +0100

    Delete file

--- src/Screens/AllPhone.js
+++ src/Screens/AllPhone.js
@@ -1,128 +1,120 @@
-import React, { Component } from 'react'
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
+import React, { Component } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Redirect } from "react-router";
+import { withRouter } from "react-router-dom";
 import axios from "axios";
-import { Tooltip } from '@material-ui/core';
+
+
 
 
 const StyledTableCell = withStyles(theme => ({
-        head: {
-                backgroundColor: theme.palette.common.black,
-                color: theme.palette.common.white,
-        },
-        body: {
-                fontSize: 14,
-        },
+	head: {
+		backgroundColor: theme.palette.common.black,
+		color: theme.palette.common.white
+	},
+	body: {
+		fontSize: 14
+	}
 }))(TableCell);
 
 const StyledTableRow = withStyles(theme => ({
-        root: {
-                '&:nth-of-type(odd)': {
-                        backgroundColor: theme.palette.background.any,
-                },
-        },
+	root: {
+		"&:nth-of-type(odd)": {
+			backgroundColor: theme.palette.background.any
+		}
+	}
 }))(TableRow);
 
 const useStyles = makeStyles(theme => ({
-        root: {
-                width: '80%',
-                marginTop: theme.spacing(3),
-                overflowX: 'auto',
-        },
-        // table: {
-        //         minWidth: 700,
-        // },
+	root: {
+		width: "80%",
+		marginTop: theme.spacing(3),
+		overflowX: "auto"
+	}
+	// table: {
+	//         minWidth: 700,
+	// },
 }));
 
+class AllPhone extends Component {
+	constructor(props) {
+		super(props);
+	}
 
- class AllPhone extends Component {
-		constructor(props) {
-                        super(props);
-                        this.deleteP = this.deleteP.bind(this);
-		}
+	state = {
+		items: [],
 
-		state = {
-                        items: [],
-                    
-			
-		};
+	};
 
-                
-                // handleChange(event) {
-		// 	//event.preventDefault();
-                // this.setState({id: event.target.value})
-                
-                        deleteP(){
-                           
-			     //  axios.delete("http://localhost:8080/phone/deletePhone/" + document.getElementById("toto").name ) 
-			        axios.delete("http://localhost:8080/phone/deletePhone/" + document.getElementById("toto").name ) 
+	delete(item) {
+		console.log("ici");
+		console.log(item);
 
+		axios
+			.delete("http://localhost:8080/phone/deletePhone/" + item._id)
 
-				.then(res => {
-					//console.log("FFYUFYFTYFTYG");
-				
-					console.log(res.data);
-				}).catch((error) =>{
-                                        console.log(error)
-                                })
-			
-                }
-                
+			.then(res => {
+				//console.log("FFYUFYFTYFTYG");
 
-                
-
-		componentDidMount() {
-			axios.get("http://localhost:8080/phone/allPhone").then(res => {
-				console.log(res);
-				this.setState({ items: res.data });
+				console.log(res.data);
+				this.componentDidMount();
+			})
+			.catch(error => {
+				console.log(error);
 			});
-		}
+	}
 
-		render() {
-			const { classes } = this.props;
-			// const item = this.state.items.map((item)=>{
-			// return <div key={item.id}>
-			//   <span>{item._id}</span> <button onClick={this.delete.bind(this, item)}>Delete</button>
-			// </div>
+	componentDidMount() {
+		axios.get("http://localhost:8080/phone/allPhone").then(res => {
+			console.log(res);
+			this.setState({ items: res.data });
+		});
+	}
 
-			//  })
 
-			return (
-				<Paper className={classes.root}>
-					{/* <div>{item}</div> */}
-					<Table className={classes.table} aria-label="customized table">
-						<TableHead>
-							<TableRow>
-								<StyledTableCell>Phone</StyledTableCell>
-								<StyledTableCell>Type</StyledTableCell>
-								<StyledTableCell>Price</StyledTableCell>
-								<StyledTableCell>Rating</StyledTableCell>
-								<StyledTableCell>Warranty</StyledTableCell>
-								<StyledTableCell>Edit</StyledTableCell>
-								<StyledTableCell>Warranty</StyledTableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{this.state.items.map(item => (
-								<StyledTableRow key={item.name}>
-									<StyledTableCell component="th" scope="row">
-										{item.name}
-									</StyledTableCell>
-									<StyledTableCell>{item.type}</StyledTableCell>
-									<StyledTableCell>{item.price}</StyledTableCell>
-									<StyledTableCell>{item.rating}</StyledTableCell>
-									<StyledTableCell>{item.warranty}</StyledTableCell>
-									<StyledTableCell>{item.warranty}</StyledTableCell>
-									<StyledTableCell>
-										{item._id}
+	render() {
+		const { classes } = this.props;
 
-										{/* <button
+
+		return (
+			<Paper className={classes.root}>
+				{/* <div>{item}</div> */}
+				<Table className={classes.table} aria-label="customized table">
+					<TableHead>
+						<TableRow>
+							<StyledTableCell>Phone</StyledTableCell>
+							<StyledTableCell>Type</StyledTableCell>
+							<StyledTableCell>Price</StyledTableCell>
+							<StyledTableCell>Rating</StyledTableCell>
+							<StyledTableCell>Warranty</StyledTableCell>
+							<StyledTableCell>Edit</StyledTableCell>
+							<StyledTableCell>Warranty</StyledTableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{this.state.items.map(item => (
+							<StyledTableRow key={item.name}>
+								<StyledTableCell component="th" scope="row">
+									{item.name}
+								</StyledTableCell>
+								<StyledTableCell>{item.type}</StyledTableCell>
+								<StyledTableCell>{item.price}</StyledTableCell>
+								<StyledTableCell>{item.rating}</StyledTableCell>
+								<StyledTableCell>{item.warranty}</StyledTableCell>
+								<StyledTableCell>
+
+								</StyledTableCell>
+								<StyledTableCell>
+									{item._id}
+
+									{/* <button
 											type="submit"
 											name={item._id}
 											id="toto"
@@ -136,28 +128,17 @@ const useStyles = makeStyles(theme => ({
 											Delete
 										</button> */}
 
-										<button
-											type="submit"
-											name={item._id}
-											id="toto"
-											//onClick={e => this.removeItem(e, item)}
-											// onClick={this.state.id}
-											onClick={() => {
-												this.deleteP(item._id);
-											}}
-											key={item._id}
-										>
-											Delete
-										</button>
-									</StyledTableCell>
-								</StyledTableRow>
-							))}
-						</TableBody>
-					</Table>
-				</Paper>
-			);
-		}
- }
+									<button key={item.id} onClick={this.delete.bind(this, item)}>
+										Delete
+									</button>
+								</StyledTableCell>
+							</StyledTableRow>
+						))}
+					</TableBody>
+				</Table>
+			</Paper>
+		);
+	}
+}
 
 export default withStyles(useStyles)(AllPhone);
-
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 import AddPhone11 from './AddPhone11'
-import AllPhonetest from './AllPhonetest'
+import AllPhone from './AllPhone'
 import Edit from './Edit'
-
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import AllPhone from './AllPhone';
 
 export default class Home extends Component {
         render() {
                 return (
                         <div className="Home">
-                                <AllPhonetest />
+                                <AllPhone />
                                 <AddPhone11 />
            
                           </div>
