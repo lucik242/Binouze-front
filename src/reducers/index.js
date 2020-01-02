5ff56b5efd1d4072aab593ff16bf632baf6dea6f
Lucie Cabelka <l.novakova.ln@gmail.com>
2020-01-02 08:28:11 +0100

    auth

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+// import {combineReducers} from 'redux';
+// import itemReducer from './itemReducer';
+
+
+
+// export default combineReducers({
+//  item: itemReducer,
+
+// });
+
+
---
+++ src/reducers/itemReducer.js
@@ -0,0 +1,17 @@
+//import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '.. /actions/types';
+import { GET_ITEMS } from '../actions/types';
+
+const initialState = {
+ items: [ ]
+ } 
+
+export default function(state = initialState, action) {
+switch(action.type){
+    case GET_ITEMS:
+      return {
+      ... state
+      }
+      default:
+     return state;
+  }
+  }
